744455bcce158e067ab635d45eb3ae61f5300015
youngkiSeo <130648198+youngkiSeo@users.noreply.github.com>
2023-05-25 14:45:54 +0900

    Add files via upload

--- board7/src/main/java/com/example/board7/fileupload/FileuploadService.java
+++ board7/src/main/java/com/example/board7/fileupload/FileuploadService.java
@@ -1,25 +1,44 @@
 package com.example.board7.fileupload;
 
-import com.example.board7.fileupload.model.FileEntity;
+import com.example.board7.fileupload.model.*;
 import com.example.board7.fileupload.model.FileuploadInsDto;
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.UUID;
 
 @Service
 public class FileuploadService {
+
+
+    @Value("${file.dir}") //파일 읽어오는
+    private String fileDir; //파일 위치 값이 fileDir 전역변수에 주입됨
     private fileuploadMapper mapper;
+    @Autowired
     public FileuploadService(fileuploadMapper mapper){
         this.mapper=mapper;
     }
 
-    @Value("${file.dir}") //파일 읽어오는
-    private String fileDir; //파일 위치 값이 fileDir 전역변수에 주입됨
+    public Resource fileLoad(FileLoadDto dto) { //Resource 파일
+        FileEntity entity = mapper.selFileById(dto);
+        try {
+            File file = new File(fileDir + entity.getPath());//getPaht 파일경로
+            Resource resource = new UrlResource(file.toURI());//file을 uri 정보로 변환시킴 url uri ... 파일경로를 객체화  파일을 resource 형식으로 객체화
+            if(resource.exists()) {//바이널 0과1로 이어진 자료를 1열로 세움 존재하면 return 시키고 아니면 return null
+                return resource;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public void fileupload(FileuploadInsDto dto, MultipartFile img) {
         System.out.println("fileDir:"+fileDir);
 
@@ -36,15 +55,15 @@ public class FileuploadService {
         String savedFileName= uuid + ext;
         String savedFilePath= fileDir + savedFileName;
         File file = new File(savedFilePath); //saveFilePath경로로 객체를 만들었음 .예외처리를 service class 에서 해야함
-        file.exists(); //파일 위치 물어보기 없으니깐 false
+        file.exists(); //파일 위치 물어보기 없으니깐 false 있으면 true
         try {
-            img.transferTo(file); //img.transferTo 파일객체를 보냄\
-            FileEntity entity =FileEntity.builder().path(savedFilePath).uploader(dto.getUploader()).levelvalue(dto.getLevelvalue()).build();
+            img.transferTo(file); //img.transferTo 파일객체를 보냄
+            FileEntity entity =FileEntity.builder().path(savedFileName).uploader(dto.getUploader()).levelvalue(dto.getLevelvalue()).build();
             //path랑 uploader랑 levelvalue 값만 넣어서 객체생성함
+            // 생성자 대신 setter 처럼 사용하는 builder 원하는 값만 넣어서 생성할수있게 해줌
             mapper.insFile(entity);
         } catch (IOException e) {
            e.printStackTrace();
         }
-
     }
 }
--- board7/src/main/java/com/example/board7/fileupload/Fileuploadontroller.java
+++ board7/src/main/java/com/example/board7/fileupload/Fileuploadontroller.java
@@ -1,10 +1,14 @@
 package com.example.board7.fileupload;
 
 import com.example.board7.board.BoardController;
+import com.example.board7.fileupload.model.FileLoadDto;
 import com.example.board7.fileupload.model.FileuploadInsDto;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +25,15 @@ public class Fileuploadontroller {
         LOGGER = LoggerFactory.getLogger(Fileuploadontroller.class);
         this.service=service;
     }
+    @GetMapping
+    public ResponseEntity<Resource> download(FileLoadDto dto) {
+        Resource file = service.fileLoad(dto);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, //header 에는 요청 응답하고 자하는 요구사항이
+                        "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file); //바디에는 전송하고자 하는 file
+    }
+
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE}) //json으로 날린다면  주소값이 필요함 consumes를 적어준다.
     public void fileupload(@RequestPart FileuploadInsDto dto, @RequestPart MultipartFile img){ //img는 무조건 @RequestPart를 적어준다  스프링이 알아서 MultipartFile 형태로 변환해준다.
         // img는 파일로 날리고 dto는 ??로 날린다
@@ -28,4 +41,6 @@ public class Fileuploadontroller {
         LOGGER.info("imgFileName:"+img.getName()); //LOGGER 찍기
         service.fileupload(dto,img); //service한테 fileupload로 전달
     }
+
+
 }
